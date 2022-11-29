07fce1bb8662ce1654e2d7ab808b74493b82fcc6
Flambrew <77941661+Flambrew@users.noreply.github.com>
2022-11-28 20:29:12 -0500

    main class shuffling

--- Main.java
+++ Main.java
@@ -5,40 +5,31 @@ import src.nodes.NumberNode;
 import src.processing.Interpreter;
 import src.processing.Lexer;
 import src.processing.Parser;
-import src.tokens.Token;
 
-import src.exceptions.*;
+import src.exceptions.IllegalOperationException;
+import src.exceptions.IllegalSyntaxException;
+import src.exceptions.IllegalTokenException;
 
 public class Main {
     public static void main(String[] args) {
-        
-        for (int i = 0; i < args.length || i < 1; i++) {
+        Lexer lx = new Lexer();
+        Parser pr = new Parser();
+        Interpreter in = new Interpreter();
 
-            String function = args.length > 0 ? args[i] : "1 + 2 - abs(3 * 4! / sin[5 ^ cos{6}]) % -tan(7)";
-            Lexer lx = new Lexer();
-            Parser pr = new Parser();
-            Interpreter in = new Interpreter();
-    
-            try {
+        try {
+            for (int i = 0; i < args.length || i < 1; i++) {
 
+                String function = args.length > 0 ? args[i] : "1 + 2 - abs(3 * 4! / sin[5 ^ cos{6}]) % -tan(7)";
                 System.out.println(function);
-    
-                Token [] tokens = lx.createTokens(function);
-                System.out.println(Arrays.deepToString(tokens));
-    
-                Node ast = pr.parse(tokens);
-                System.out.println(ast.toString());
-
-                NumberNode out = in.calculate(ast);
-                System.out.printf("%f", out.VALUE);
-    
-            } catch (IllegalTokenException e) {
-                e.printStackTrace();
-            } catch (IllegalSyntaxException e) {
-                e.printStackTrace();
-            } catch (IllegalOperationException e) {
-                e.printStackTrace();
+                System.out.printf("%f", in.calculate(pr.parse(lx.createTokens(function))).VALUE);
+                
             }
+        } catch (IllegalTokenException e) {
+            e.printStackTrace();
+        } catch (IllegalSyntaxException e) {
+            e.printStackTrace();
+        } catch (IllegalOperationException e) {
+            e.printStackTrace();
         }
     }
 }
