f0237b455b80632597ebb20de606f6a4dde75150
Flambrew <77941661+Flambrew@users.noreply.github.com>
2022-11-28 22:07:50 -0500

    added output pane to jar file

--- src/_main/Main.java
+++ src/_main/Main.java
@@ -4,8 +4,6 @@ import src.processing.Interpreter;
 import src.processing.Lexer;
 import src.processing.Parser;
 
-import java.util.Arrays;
-
 import javax.swing.JOptionPane;
 
 import src.exceptions.IllegalOperationException;
@@ -19,13 +17,8 @@ public class Main {
         Interpreter in = new Interpreter();
 
         try {
-
             String function = args.length > 0 ? args[0] : JOptionPane.showInputDialog("Input equation:");
-            function = !function.equals("") ? function : "1 + 2 - abs(3 * 4! / sin[5 ^ cos{6}]) % -tan(7)";
-
-            System.out.println(function);
-            System.out.println(Arrays.deepToString(lx.createTokens(function)));
-            System.out.println(pr.parse(lx.createTokens(function)));
+            JOptionPane.showMessageDialog(null, String.format("%s = %f", function, in.calculate(pr.parse(lx.createTokens(function))).VALUE));
 
             System.out.printf("%s = %f", function, in.calculate(pr.parse(lx.createTokens(function))).VALUE);
 
